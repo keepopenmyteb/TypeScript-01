ecbe9a749247d9f13b0a7b7ec4160ddf5b52ea63
user <oprenber@gmail.com>
2026-01-02 13:08:27 +0900

    typescript 연습

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+let myString: string = "My String";
+console.log("String:", myString);
+
+let myNumber: number = 7;
+console.log("Number:", myNumber);
+
+let myBoolean: boolean = true;
+console.log("Boolean:", myBoolean);
+
+let myNull: null = null;
+console.log("Null:", myNull);
+
+let myAny: any = "모든 타입 허용";
+console.log("Any string:", myAny);
+
+myAny = 5;
+console.log("changed any to number:", myAny);
+
+myAny = false;
+console.log("changed any to boolean:", myAny);
